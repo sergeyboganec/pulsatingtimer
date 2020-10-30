b3ae8f78645671d6dc0b1fdb4224d1774aa4494e
Sergey Bogdanec <sweid531@gmail.com>
2020-10-30 11:24:15 +0300

    Implemented pulsating effect

--- pulsatingtimer/src/main/java/com/sergeybogdanec/pulsatingtimer/PulsatingTimer.kt
+++ pulsatingtimer/src/main/java/com/sergeybogdanec/pulsatingtimer/PulsatingTimer.kt
@@ -5,6 +5,7 @@ import android.content.res.Resources
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Typeface
 import android.os.Handler
 import android.os.Looper
 import android.text.TextPaint
@@ -13,7 +14,7 @@ import android.util.Log
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.content.res.use
-import kotlin.math.log
+import java.io.Serializable
 import kotlin.math.min
 import kotlin.math.round
 
@@ -22,15 +23,16 @@ class PulsatingTimer @JvmOverloads constructor(
     attrs: AttributeSet? = null
 ): View(context, attrs) {
 
-    private val circlePaint = Paint().apply {
-        color = Color.CYAN
-    }
+    private val circlePaint = Paint()
+
+    private val pulsationPaint = Paint()
+
     private val textPaint = TextPaint(Paint.ANTI_ALIAS_FLAG).apply {
         textAlign = Paint.Align.CENTER
     }
 
     private var _max = DEFAULT_MAX
-    var max
+    var max: Int
         get() = _max
         set(value) {
             _max = value
@@ -42,7 +44,7 @@ class PulsatingTimer @JvmOverloads constructor(
             field = value
             textPaint.color = value
         }
-    var textColor
+    var textColor: Int
         get() = _textColor
         set(value) {
             _textColor = value
@@ -54,7 +56,7 @@ class PulsatingTimer @JvmOverloads constructor(
             field = value
             circlePaint.color = value
         }
-    var backgroundTint
+    var backgroundTint: Int
         get() = _backgroundTint
         set(value) {
             _backgroundTint = value
@@ -62,7 +64,7 @@ class PulsatingTimer @JvmOverloads constructor(
         }
 
     private var _progress = DEFAULT_PROGRESS
-    var progress
+    var progress: Int
         get() = _progress
         set(value) {
             _progress = value
@@ -74,7 +76,7 @@ class PulsatingTimer @JvmOverloads constructor(
             field = value
             textPaint.textSize = value
         }
-    var textSize
+    var textSize: Float
         get() = _textSize
         set(value) {
             _textSize = value
@@ -86,7 +88,7 @@ class PulsatingTimer @JvmOverloads constructor(
             field = value
             textPaint.typeface = value
         }
-    var typeface
+    var typeface: Typeface
         get() = _typeface
         set(value) {
             _typeface = value
@@ -94,10 +96,46 @@ class PulsatingTimer @JvmOverloads constructor(
         }
 
     private var _circleRadius = -1f
-    var circleRadius
+    var circleRadius: Float
         get() = _circleRadius
         set(value) {
             _circleRadius = value
+            invalidate()
+        }
+
+    private var _pulsationAlpha = DEFAULT_PULSATION_ALPHA
+    var pulsationAlpha: Float
+        get() = _pulsationAlpha
+        set(value) {
+            _pulsationAlpha = value
+            invalidate()
+        }
+
+    private var _pulsationInterval = DEFAULT_PULSATION_INTERVAL
+    var pulsationInterval: Long
+        get() = _pulsationInterval
+        set(value) {
+            _pulsationInterval = value
+            invalidate()
+        }
+
+    private var _pulsationDuration = DEFAULT_PULSATION_DURATION
+    var pulsationDuration: Long
+        get() = _pulsationDuration
+        set(value) {
+            _pulsationInterval = value
+            invalidate()
+        }
+
+    private var _pulsationColor = DEFAULT_PULSATION_COLOR
+        set(value) {
+            field = value
+            pulsationPaint.color = value
+        }
+    var pulsationColor: Int
+        get() = _pulsationColor
+        set(value) {
+            _pulsationColor
             invalidate()
         }
 
@@ -109,6 +147,10 @@ class PulsatingTimer @JvmOverloads constructor(
             _progress = typedArray.getInt(R.styleable.PulsatingTimer_android_progress, DEFAULT_PROGRESS)
             _textSize = typedArray.getDimension(R.styleable.PulsatingTimer_android_textSize, 0f)
             _circleRadius = typedArray.getDimension(R.styleable.PulsatingTimer_circleRadius, -1f)
+            _pulsationAlpha = typedArray.getDimension(R.styleable.PulsatingTimer_pulsationAlpha, DEFAULT_PULSATION_ALPHA)
+            _pulsationInterval = typedArray.getInt(R.styleable.PulsatingTimer_pulsationIntervalMillis, DEFAULT_PULSATION_INTERVAL.toInt()).toLong()
+            _pulsationDuration = typedArray.getInt(R.styleable.PulsatingTimer_pulsationDuration, DEFAULT_PULSATION_DURATION.toInt()).toLong()
+            _pulsationColor = typedArray.getColor(R.styleable.PulsatingTimer_pulsationColor, DEFAULT_PULSATION_COLOR)
             try {
                 val resId = typedArray.getResourceId(R.styleable.PulsatingTimer_android_fontFamily, -1)
                 if (resId != -1) {
@@ -122,19 +164,36 @@ class PulsatingTimer @JvmOverloads constructor(
         }
     }
 
-    private val timerHandler: Handler = Handler(Looper.getMainLooper())
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        pulsationTargetRadius = min(w, h) / 2
+    }
 
-    private var drawCount = 0
+    private var pulsationTargetRadius: Int = 0
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawColor(Color.TRANSPARENT)
 
-        Log.d(TAG, "onDraw: ${++drawCount}")
-
         val centerX = round(width / 2f)
         val centerY = round(height / 2f)
         val radius = circleRadius.takeIf { it >= 0 } ?: min(centerX, centerY)
+
+        val currentTime = System.currentTimeMillis()
+        val radiusDiff = pulsationTargetRadius - radius
+
+        val iterator = pulsations.iterator()
+        while(iterator.hasNext()) {
+            val time = iterator.next()
+            val fraction = (currentTime - time).toFloat() / _pulsationDuration
+            if (fraction < MAX_FRACTION) {
+                val pulsationRadius = radiusDiff * fraction + radius
+                pulsationPaint.alpha = ((pulsationAlpha * (1f - fraction)) * MAX_ALPHA).toInt()
+                canvas.drawCircle(centerX, centerY, pulsationRadius, pulsationPaint)
+            } else {
+                iterator.remove()
+            }
+        }
 
         canvas.drawCircle(centerX, centerY, radius, circlePaint)
 
@@ -143,18 +202,7 @@ class PulsatingTimer @JvmOverloads constructor(
 
         canvas.drawText(text, centerX, ty, textPaint)
 
-        if (hasWindowFocus()) invalidate()
-    }
-
-    fun start() {
-        timerHandler.removeCallbacksAndMessages(null)
-        timerHandler.postDelayed(::handlerRunnable, ONE_SECOND)
-        pulsatingListener?.onStart()
-    }
-
-    fun pause() {
-        timerHandler.removeCallbacksAndMessages(null)
-        pulsatingListener?.onPause()
+        if (pulsations.isNotEmpty()) invalidate()
     }
 
     private var pulsatingListener: PulsatingListener? = null
@@ -163,15 +211,36 @@ class PulsatingTimer @JvmOverloads constructor(
         pulsatingListener = listener
     }
 
-    private fun handlerRunnable() {
+    private val animationHandler: Handler by lazy { Handler(Looper.getMainLooper()) }
+
+    private val pulsations: MutableList<Long> by lazy { mutableListOf() }
+
+    fun start() {
+        animationHandler.removeCallbacksAndMessages(null)
+        animationHandler.postDelayed(::timerRunnable, ONE_SECOND)
+        animationHandler.postDelayed(::pulsationRunnable, _pulsationInterval)
+        pulsatingListener?.onStart()
+    }
+
+    fun pause() {
+        animationHandler.removeCallbacksAndMessages(null)
+        pulsatingListener?.onPause()
+    }
+
+    private fun timerRunnable() {
         if (_progress < _max) {
-            Log.d(TAG, "handlerRunnable: $progress")
             progress += 1
             pulsatingListener?.onUpdate(_progress)
-            timerHandler.postDelayed(::handlerRunnable, ONE_SECOND)
+            animationHandler.postDelayed(::timerRunnable, ONE_SECOND)
         } else {
+            animationHandler.removeCallbacksAndMessages(null)
             pulsatingListener?.onEnd()
         }
+    }
+
+    private fun pulsationRunnable() {
+        pulsations.add(System.currentTimeMillis())
+        animationHandler.postDelayed(::pulsationRunnable, _pulsationInterval)
     }
 
     companion object {
@@ -180,9 +249,17 @@ class PulsatingTimer @JvmOverloads constructor(
         private const val DEFAULT_PROGRESS = 0
         private const val DEFAULT_TEXT_COLOR = Color.BLACK
         private const val DEFAULT_BACKGROUND_TINT = Color.WHITE
+        private const val DEFAULT_PULSATION_COLOR = Color.WHITE
+        private const val DEFAULT_PULSATION_ALPHA = .5f
+        private const val DEFAULT_PULSATION_INTERVAL = 1000L
+        private const val DEFAULT_PULSATION_DURATION = 1200L
 
         private const val ONE_SECOND = 1000L
+        private const val MAX_FRACTION = 1f
+        private const val MAX_ALPHA = 255
+
         private const val TAG = "PulsatingTimer"
 
     }
 }
+
